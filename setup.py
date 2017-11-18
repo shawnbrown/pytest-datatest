666750b145b358bb7d1dc3792baccd250be1a3f8
Shawn Brown <shawnbrown@users.noreply.github.com>
2017-11-18 15:34:00 -0500

    Initial commit.
        "...our ability to analyze data has grown far more
         sophisticated than our thinking about what we ought
         to do with the results." -Charlie Wheelan

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-datatest',
+    version='0.1.0',
+    author='Shawn Brown',
+    maintainer='Shawn Brown',
+    license='MIT',
+    url='https://github.com/shawnbrown/pytest-datatest',
+    description='A pytest plugin for datatest and test driven data-wrangling',
+    long_description=read('README.rst'),
+    py_modules=['pytest_datatest'],
+    install_requires=['pytest>=3.1.1'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'datatest = pytest_datatest',
+        ],
+    },
+)
