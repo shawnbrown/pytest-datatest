2e1564d042cf07f599f5406324e66b20fbd082e9
Shawn Brown <shawnbrown@users.noreply.github.com>
2017-11-22 16:12:32 -0500

    Add DatatestReprEntry class that will be used to change the behavior of the toterminal() method.

--- pytest_datatest.py
+++ pytest_datatest.py
@@ -1,1 +1,19 @@
 # -*- coding: utf-8 -*-
+
+from _pytest._code.code import ReprEntry
+
+
+class DatatestReprEntry(ReprEntry):
+    """Wrapper for ReprEntry to change behavior of toterminal() method."""
+    def __init__(self, entry):
+        if not isinstance(entry, ReprEntry):
+            cls_name = entry.__class__.__name__
+            raise ValueError('expected ReprEntry, got {0}'.format(cls_name))
+
+        super(DatatestReprEntry, self).__init__(
+            getattr(entry, 'lines', []),
+            getattr(entry, 'reprfuncargs', None),
+            getattr(entry, 'reprlocals', None),
+            getattr(entry, 'reprfileloc', None),
+            getattr(entry, 'style', None),
+        )
--- tests/test_datatest.py
+++ tests/test_datatest.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+from _pytest._code.code import ReprEntry
+from _pytest._code.code import ReprFuncArgs
+from _pytest._code.code import ReprFileLocation
 
-def test_initial(testdir):
-    pass
+from pytest_datatest import DatatestReprEntry
+
+
+def test_DatatestReprEntry(testdir):
+    lines = [
+        '    def test_foo():',
+        '>       assert 1 == 2',
+        'E       assert 1 == 2',
+    ]
+    reprfuncargs = ReprFuncArgs([])
+    reprlocals = None
+    reprfileloc = ReprFileLocation('test_script.py', 9, 'AssertionError')
+    style = 'long'
+
+    original = ReprEntry(lines, reprfuncargs, reprlocals, reprfileloc, style)
+    wrapped = DatatestReprEntry(original)
+
+    assert isinstance(wrapped, ReprEntry), 'should be derived from ReprEntry'
+    assert wrapped.lines == original.lines
+    assert wrapped.reprfuncargs == original.reprfuncargs
+    assert wrapped.reprlocals == original.reprlocals
+    assert wrapped.reprfileloc == original.reprfileloc
+    assert wrapped.style == original.style
