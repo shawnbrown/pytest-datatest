49a7b9c202ee7f845a4ceb92da9217817e28471b
Shawn Brown <shawnbrown@users.noreply.github.com>
2017-11-24 13:38:58 -0500

    Move function into class to group related tests together.

--- tests/test_datatest.py
+++ tests/test_datatest.py
@@ -7,23 +7,37 @@ from _pytest._code.code import ReprFileLocation
 from pytest_datatest import DatatestReprEntry
 
 
-def test_DatatestReprEntry(testdir):
-    lines = [
-        '    def test_foo():',
-        '>       assert 1 == 2',
-        'E       assert 1 == 2',
-    ]
-    reprfuncargs = ReprFuncArgs([])
-    reprlocals = None
-    reprfileloc = ReprFileLocation('test_script.py', 9, 'AssertionError')
-    style = 'long'
+class DummyTerminalWriter(object):
+    """Helper class for testing calls to line() and write() methods."""
+    def __init__(self):
+        self.all_lines = []
 
-    original = ReprEntry(lines, reprfuncargs, reprlocals, reprfileloc, style)
-    wrapped = DatatestReprEntry(original)
+    def line(self, line, **kwds):
+        self.all_lines.append((line, kwds))
 
-    assert isinstance(wrapped, ReprEntry), 'should be derived from ReprEntry'
-    assert wrapped.lines == original.lines
-    assert wrapped.reprfuncargs == original.reprfuncargs
-    assert wrapped.reprlocals == original.reprlocals
-    assert wrapped.reprfileloc == original.reprfileloc
-    assert wrapped.style == original.style
+    def write(self, line, **kwds):
+        self.all_lines.append((line, kwds))
+
+
+class TestDatatestReprEntry(object):
+    def test_instantiation(self):
+        lines = [
+            '    def test_foo():',
+            '>       assert 1 == 2',
+            'E       assert 1 == 2',
+        ]
+        reprfuncargs = ReprFuncArgs([])
+        reprlocals = None
+        reprfileloc = ReprFileLocation('test_script.py', 9, 'AssertionError')
+        style = 'long'
+
+        original = ReprEntry(
+            lines, reprfuncargs, reprlocals, reprfileloc, style)
+        wrapped = DatatestReprEntry(original)
+
+        assert isinstance(wrapped, ReprEntry), 'should derive from ReprEntry'
+        assert wrapped.lines == original.lines
+        assert wrapped.reprfuncargs == original.reprfuncargs
+        assert wrapped.reprlocals == original.reprlocals
+        assert wrapped.reprfileloc == original.reprfileloc
+        assert wrapped.style == original.style
